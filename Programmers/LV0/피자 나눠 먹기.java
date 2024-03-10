3e83d36250353fd69abe66ac2f61650110af5ea4
kds98 <skyudae@naver.com>
2024-03-10 23:58:00 +0900

    [PGS] 피자 나눠 먹기 / LV0

---
+++ Programmers/LV0/피자 나눠 먹기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        if(n % slice == 0) {
+            answer = n / slice;
+        }
+        else if(n % slice != 0) {
+            if(slice < n) {
+                answer = n / slice + 1;
+            }
+            else if(slice >= n) {
+                answer = 1;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
