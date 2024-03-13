228a168061ba52bd059df944e381a3ecbb2767b7
kds98 <skyudae@naver.com>
2024-03-13 22:43:59 +0900

    [PGS] 문자열 안에 문자열 / LV0

--- Programmers/LV0/문자열 안에 문자열.java
+++ Programmers/LV0/문자열 안에 문자열.java
@@ -1,13 +1,15 @@
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
+        boolean check = true;
         
-        if(str1.contains(str2)) {
-			answer = 1;
-		} else {
-			answer = 2;
-		}
+        check = str1.contains(str2);
         
+        if(check == true) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
         
         return answer;
     }
